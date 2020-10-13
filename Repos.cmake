ec1e51daad5bbdb916a3bf5725f1a3ce1bd0bb51
Jared Duffey <jared.duffey@bluequartz.net>
2020-10-13 14:02:32 -0400

    Changed CI to use CMake to drive checkout of repositories
    Signed-off-by: Jared Duffey <jared.duffey@bluequartz.net>

---
+++ Repos.cmake
@@ -0,0 +1,20 @@
+set(EMSOFT_REPOS
+  EMsoft
+  EMsoftData
+)
+
+#--------------------------------------------------------------------------------------------------
+# EMsoft Details
+#--------------------------------------------------------------------------------------------------
+set(EMsoft_REPO_NAME "EMsoft")
+set(EMsoft_BASE_URL "https://www.github.com")
+set(EMsoft_GH_ORG "EMsoft-org")
+set(EMsoft_GIT_BRANCH "develop")
+
+#--------------------------------------------------------------------------------------------------
+# EMsoftData Details
+#--------------------------------------------------------------------------------------------------
+set(EMsoftData_REPO_NAME "EMsoftData")
+set(EMsoftData_BASE_URL "https://www.github.com")
+set(EMsoftData_GH_ORG "EMsoft-org")
+set(EMsoftData_GIT_BRANCH "develop")
